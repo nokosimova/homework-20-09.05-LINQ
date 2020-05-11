cb996f6a4b2ba968c03f8636764bd8d4c2675fb8
kosimovanodira <nkosimova@gmail.com>
2020-05-11 22:26:32 +0500

    Добавьте файлы проекта.

---
+++ hw20-LINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hw20_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 2 :  ");
+            Console.WriteLine("Задание 3 :  ");
+            Console.WriteLine("Задание 4 :  ");
+            Console.WriteLine("Задание 5 :  ");
+        }
+    }
+}
