6086f0321e1b2861d7950e390a534871c93faddb
kosimovanodira <nkosimova@gmail.com>
2020-05-14 18:33:56 +0500

    fix all corrections

--- hw20-LINQ/Program.cs
+++ hw20-LINQ/Program.cs
@@ -9,83 +9,49 @@ namespace hw20_LINQ
     class Program
     {
         static void Main(string[] args)
-        {   //**************//
+        {
+            
+            //**************//
             Console.WriteLine("Задание 2.1 :  ");
-            int ind = 0;
-            List<Pair<int,char>> list = new List<Pair<int, char>>();
-            string num = (new Random().Next(10000, 1000000)).ToString();            
-            for (int i = 0; i < num.Length; i++)
-                list.Add(new Pair<int, char>(--ind, num[i]));
-            var answer = from i in list
-                         orderby i.index
-                         select i;
+            int number = new Random().Next(10000, 1000000);
+            var answer = number.ToString().ToCharArray().Reverse();
 
-            Console.WriteLine($"число: {num}");
-            Console.Write("ответ: ");
-            foreach (var i in answer)
-                Console.Write($"{i.value} ");
-            Thread.Sleep(3000);
+            Console.WriteLine($"число: {number}");
+            Console.Write("результат: [");
+            foreach (var item in answer)
+                Console.Write($"{item} ");
+            Console.WriteLine("]");
 
             //***************//
-            Console.WriteLine("\nЗадание 2.2 :  ");
+            Console.WriteLine("Задание 2.2 :  ");
             Console.WriteLine("Введите через пробел массив целых чисел:");
             string[] array = Console.ReadLine().Split();
             List<int> answerList = new List<int>();
 
-            int arrayCount = (from i in array where (int.Parse(i) > 0) select i).Count();
-            int sumOfNegative = array.Where(i => (int.Parse(i) < 0)).Sum(i => int.Parse(i));            
-            answerList.Add(arrayCount);
-            answerList.Add(sumOfNegative);
+            int positiveCount = array.Where(i => int.Parse(i) > 0).Count();
+            int negativeSum = array.Where(i => (int.Parse(i) < 0)).Sum(i => int.Parse(i));
+            answerList.Add(positiveCount);
+            answerList.Add(negativeSum);
 
-            Console.Write("ответ: [");
-            foreach (int i in answerList)
-                Console.Write($"{i} ");
-            Console.Write("]");
-
+            Console.WriteLine($"ответ: [{answerList[0]},  {answerList[1]}]");
+          
             //**************//
             Console.WriteLine("\nЗадание 2.3 :  ");
             Console.WriteLine("Введите через пробел несколько слов:");
             string[] inputStream = Console.ReadLine().Split();
-            List<Pair<int, string>> wordList = new List<Pair<int, string>>();
-            foreach (var i in inputStream)
-                wordList.Add(new Pair<int,string>(i.Length, i));
-                
-            var sortedWordList = from i in wordList
-                                 orderby i.index
-                                 select i;
-            foreach (var i in sortedWordList)
-                Console.Write($"{i.value} ");
+            var answer2 = inputStream.OrderBy(i => i.Length);
+            Console.Write("ответ: ");
+            foreach (var i in answer2)
+                Console.Write($"{i} ");
 
             //**************//
             Console.WriteLine("\nЗадание 2.4 :  ");
             Console.WriteLine("Введите через пробел массив целых чисел в котором все элементы равны, кроме одного:");
-            string[] inputStream2 = Console.ReadLine().Split();
-            List<int> numberList = new List<int>();
-            int ans;
-
-            foreach(var i in inputStream2)
-                numberList.Add(int.Parse(i));
-            numberList.Sort();
-            int minNum = numberList.Min();
-            int maxNum = numberList.Max();
-
-            if (numberList[1] == minNum) ans = maxNum;
-            else ans = minNum;
-           // var ans = numbetList.Distinct();
-            Console.Write($"белая ворона - это число {ans}");
-
+            string[] arr = Console.ReadLine().Split();
+            var answer3 = arr.First(i => i != (arr.First())) == arr.Last(i => i != (arr.First())) ?
+                arr.First(i => i != (arr.First())) : arr.First();
+            Console.Write($"белая ворона - это число {answer3}");
             Console.ReadKey();
-        }
-
-        public class Pair<T,V>
-        {
-            public T index { get; set;}
-            public V value { get; set; }
-            public Pair(T i, V v)
-            {
-                index = i;
-                value = v;
-            }
         }
     }
 }
